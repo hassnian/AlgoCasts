75b22f076bd7754d0c6c32d9af5cc6dc20b8e86d
Hassnian <hassnianxd@gmail.com>
2019-04-28 14:52:35 +0200

    pallindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split("").every((char,i)=>{
+        return char ===str[str.length-i-1]
+    })
+    
+}
 
 module.exports = palindrome;
+
+
+// function palindrome(str) {
+//     let revStr=str.split("").reduce((rev,char)=>char+rev,"")
+//     return revStr==str;
+// }
