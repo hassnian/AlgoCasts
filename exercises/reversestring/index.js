29540d74d6d18227939d7d8f168d03a6f56d094b
Hassnian <hassnianxd@gmail.com>
2019-04-28 14:52:24 +0200

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+   return str.split("").reduce((rev,char)=>char+rev,"")
+}
+
+
+
 
 module.exports = reverse;
+
+//1
+// function reverse(str) {
+//     return str.split("").reverse().join("")
+//   }
+
+//2
+// function reverse(str) {
+//     let reversed = '';
+//     for(let char of str){
+//         reversed=char+reversed
+//     }
+//   return reversed;
+// }
