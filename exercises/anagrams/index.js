ca52e80076733d3a4c946bdf31cbbbe3b6ffde13
Hassnian <hassnianxd@gmail.com>
2019-05-04 12:51:31 +0200

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB){
+        const strA=cleaString(stringA);
+        const strB=cleaString(stringB);
+        return strA===strB?  true:  false
+}
+function cleaString(str){
+    return str.replace(/[^\w]/g,'').toLowerCase().split("").sort().join("")
+}
 
+anagrams('rail safety', 'fairy tales')
 module.exports = anagrams;
+
+
+// function anagrams(stringA, stringB) {
+        
+//     const charMapA=buildCharMap(stringA)
+//     const charMapB=buildCharMap(stringB)
+//     console.log();
+//     console.log(charMapB);
+// if(Object.keys(charMapA).length !== Object.keys(charMapB).length){
+//     return false
+// }
+// for (let char in charMapA){
+//     if(charMapA[char]!==charMapB[char]){
+//         return false
+//     }
+    
+// }
+// return true
+
+// }
+// function buildCharMap(string){
+// let charMap={}
+// for (let char of string.replace(/[^\w]/g,'').toLowerCase()){
+//    charMap[char]=charMap[char]++ || 1
+// } 
+// return charMap
+
+// }
